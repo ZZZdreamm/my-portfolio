f9f13f3b01de90ea5a7df3d8cbb8013f37ba8bac
ZZZdream <kmultan1234@gmail.com>
2023-07-15 14:31:07 +0200

    added new project to the list and images for this

--- src/pages/MyProjects/index.tsx
+++ src/pages/MyProjects/index.tsx
@@ -8,14 +8,12 @@ import ImageFollower from "../../components/ImageFollower";
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.defaults({ ease: "none", duration: 1 });
-ScrollTrigger.normalizeScroll(true)
-
+ScrollTrigger.normalizeScroll(true);
 
 const myProjects = [
   {
     icon: "kahoot-icon.webp",
     name: "Kahoot like game",
-    side: "left",
     link: "https://zzzdreamm.github.io/Cacarrot/",
     description:
       "A game that allows you to create and play quizzes with others",
@@ -24,41 +22,23 @@ const myProjects = [
   {
     icon: "social-media-icon.png",
     name: "Social media app",
-    side: "right",
     link: "https://zzzdreamm.github.io/social-app/",
     description:
       "A social media app that allows you to post and comment, like, dislike, and delete posts.",
     hoverImage: ["friendLink-1.png", "friendLink-2.jpg", "friendLink-3.jpg"],
   },
+  {
+    icon: "news-icon.png",
+    name: "News app",
+    link: "https://hot-news-cyan.vercel.app/",
+    description:
+      "A news app which allows searching internet for most recent news on topic of your choice.",
+    hoverImage: ["hot-news-1.png", "hot-news-2.png", "hot-news-3.png"],
+  },
 ];
 export default function MyProjects() {
   const [hoveredImage, setHoveredImage] = useState<string[]>([]);
   useEffect(() => {
-    // let tl = gsap.timeline({
-    //   // yes, we can add it to an entire timeline!
-    //   scrollTrigger: {
-    //     trigger: ".my-projects",
-    //     pin: true, // pin the trigger element while active
-    //     start: "top top", // when the top of the trigger hits the top of the viewport
-    //     end: "top 100px", // end after scrolling 500px beyond the start
-    //     scrub: true, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-    //     snap: {
-    //       snapTo: "labels", // snap to the closest label in the timeline
-    //       duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-    //       delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-    //       ease: "power1.inOut", // the ease of the snap animation ("power3" by default)
-    //     },
-    //   },
-    // });
-
-
-
-
-
-    // const tl = gsap.timeline();
-    // tl.from("project-1", { yPercent: 100 }).from("project-2", {
-    //   yPercent: 100,
-    // });
     let sections = gsap.utils.toArray(".project");
 
     gsap.to(sections, {
@@ -75,10 +55,6 @@ export default function MyProjects() {
         end: () => "+=" + document.querySelector(".my-projects").offsetWidth,
       },
     });
-
-
-
-
   }, []);
 
   return (
@@ -91,8 +67,8 @@ export default function MyProjects() {
           gridTemplateColumns: `repeat(${myProjects.length}, 100vw)`,
         }}
       >
-        {myProjects && myProjects.map((project) => (
-          <>
+        {myProjects &&
+          myProjects.map((project) => (
             <Project
               key={project.name}
               icon={project.icon!}
@@ -102,8 +78,7 @@ export default function MyProjects() {
               setHoveredImage={setHoveredImage}
               hoverImage={project.hoverImage}
             />
-          </>
-        ))}
+          ))}
         <div
           style={{ height: "7rem" }}
           className="full-width very-large-font bold-font"
