b40813d93992e4e154e6ad8a457b90c4fa834221
ZZZdream <kmultan1234@gmail.com>
2023-07-18 17:34:56 +0200

    portal changed

--- src/utils/Portal/index.tsx
+++ src/utils/Portal/index.tsx
@@ -1,8 +1,9 @@
 import ReactDOM from "react-dom";
 
 export default function Portal({ children }: any) {
+  if(!document.getElementById('scroll-container')) return <>{children}</>
   if (typeof document == "undefined") {
     return <>{children}</>;
   }
-  return ReactDOM.createPortal(children, document.body);
+  return ReactDOM.createPortal(children, document.getElementById('scroll-container')!);
 }
