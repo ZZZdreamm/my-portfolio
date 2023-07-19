8290905dfe4ab536d8869a67a6ac9e905e8ba201
ZZZdream <kmultan1234@gmail.com>
2023-07-19 19:48:55 +0200

    Added direct portal to body

--- src/utils/Portal/index.tsx
+++ src/utils/Portal/index.tsx
@@ -1,9 +1,19 @@
 import ReactDOM from "react-dom";
 
 export default function Portal({ children }: any) {
-  if(!document.getElementById('scroll-container')) return <>{children}</>
+  if (!document.getElementById("scroll-container")) return <>{children}</>;
   if (typeof document == "undefined") {
     return <>{children}</>;
   }
-  return ReactDOM.createPortal(children, document.getElementById('scroll-container')!);
+  return ReactDOM.createPortal(
+    children,
+    document.getElementById("scroll-container")!
+  );
+}
+
+export function BodyPortal({ children }: any) {
+  if (typeof document == "undefined") {
+    return <>{children}</>;
+  }
+  return ReactDOM.createPortal(children, document.body);
 }
