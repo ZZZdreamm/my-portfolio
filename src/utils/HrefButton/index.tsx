c8718e57150277bb25dd11c214cd341c907554c2
ZZZdream <kmultan1234@gmail.com>
2023-07-20 17:45:12 +0200

    Cool sliding arrow animation on HrefButton

--- src/utils/HrefButton/index.tsx
+++ src/utils/HrefButton/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { ReadyImagesURL } from "../../publicPaths";
 import "./style.scss";
 
@@ -6,18 +7,38 @@ interface HrefButtonProps {
 }
 
 export default function HrefButton({ link }: HrefButtonProps) {
+  const hrefButtonRef = useRef<HTMLSpanElement>(null);
+  const firstArrowRef = useRef<HTMLImageElement>(null);
+  const secondArrowRef = useRef<HTMLImageElement>(null);
   function handleClick() {
     if (link) {
       window.open(link, "_blank");
     }
   }
+  useEffect(() => {
+    if (
+      !hrefButtonRef.current &&
+      !firstArrowRef.current &&
+      !secondArrowRef.current
+    )
+      return;
+    hrefButtonRef.current?.addEventListener("mouseenter", () => {
+      firstArrowRef.current!.style.transform = "rotate(45deg) translate(0%, -300%)";
+      secondArrowRef.current!.style.transform = "rotate(45deg) translate(0%, 0%)";
+    });
+    hrefButtonRef.current?.addEventListener("mouseleave", () => {
+      firstArrowRef.current!.style.transform = "rotate(45deg) translate(0%, 0%)";
+      secondArrowRef.current!.style.transform = "rotate(45deg) translate(0%, 300%)";
+    });
+  }, []);
 
   return (
-    <span className="hrefButton">
+    <span ref={hrefButtonRef} className="hrefButton" onClick={handleClick}>
+      <img ref={firstArrowRef} src={`${ReadyImagesURL}/svg-arrow.svg`} alt="" />
       <img
-        style={{ transform: "rotate(45deg)" }}
+        ref={secondArrowRef}
+        style={{ transform: "translate(-100%, 300%) rotate(45deg)" }}
         src={`${ReadyImagesURL}/svg-arrow.svg`}
-        onClick={handleClick}
         alt=""
       />
     </span>
